e31f84dd4b2a6af26c42481c0dd99fe061411bbd
Prashant Girennavar <giteepag@yahoo.co.in>
2016-02-18 07:16:35 +0100

    updated home page with image and finalized the project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217094835) do
+ActiveRecord::Schema.define(version: 20160218052856) do
 
   create_table "electricalexpenses", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20160217094835) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest", limit: 255
+    t.boolean  "admin",                       default: false
   end
 
 end
